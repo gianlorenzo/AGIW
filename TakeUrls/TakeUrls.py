30e4980bed049c05220390334068b5897c97d6e2
gianlorenzo.didonato@gmail.com <Fischietto93$>
2018-04-01 12:07:31 +0200

    fix

--- TakeUrls/TakeUrls.py
+++ TakeUrls/TakeUrls.py
@@ -1,15 +1,11 @@
 import json
-from pprint import pprint
 
 #Legge file .json. Ritorna un dizionario con {nome sito web, array di url}
 def readJson():
     return json.load(open('/home/gianlorenzo/AGIW/dexter_urls_category_notebook.json'))
 
-#Scrive in un array tutte le chiavi del dizionario
+#Ritorna tutte le chiavi del dizionario
 def takeAllKeys():
-    keys = []
-    for i in readJson():
-        keys.append(i)
-    return keys
+   return list(readJson())
 
 
