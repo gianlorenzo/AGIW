89b64ab6807d29fd14dee38b6d139413ec93750e
gianlorenzo.didonato@gmail.com <Fischietto93$>
2018-04-04 17:05:53 +0200

    manca solo http error

--- TakeUrls/TakeUrls.py
+++ TakeUrls/TakeUrls.py
@@ -1,9 +1,16 @@
 import json
 import urllib.request
+from urllib.request import (urlretrieve,urlunparse,urlparse)
 import urllib.error
 from bs4 import BeautifulSoup
+import requests
+from urllib import error
+import cssutils
+
 import os
-import requests.exceptions
+from cssselect import GenericTranslator, SelectorError
+
+import cssutils
 #Legge file .json. Ritorna un dizionario con {nome sito web, array di url}
 def readJson():
     return json.load(open('/home/gianlorenzo/AGIW/dexter_urls_category_notebook.json'))
@@ -27,8 +34,8 @@ def writeHtmlFile(url):
     try:
         r = takeHtmlContent(url)
     except urllib.error.URLError as e:
-        f = open("error.html","w+")
-        f.write(str(e.reason))
+        f = open("error.txt","w+")
+        f.write("porcaeva")
         f.close()
     else:
         f = open("noError.html","w+")
@@ -39,7 +46,26 @@ def writeHtmlFile(url):
 urlBuona = "https://www.ecrater.com/p/2042546/toshiba-satellite-a40-series-recovery"
 urlCattiva = "http://53laptop.com/product/505_hp+touchsmart+tx2+121+inch+notebook"
 
-writeHtmlFile("http://53laptop.com/product/505_hp+touchsmart+tx2+121+inch+notebook")
+def takeCss(url):
+    r = takeHtmlContent(url)
+    css_url = [link["href"] for link in r.findAll("link") if "stylesheet" in link.get("rel",[])]
+    for x in css_url:
+        y = x.split('/',-1)[-1]
+        outpath = os.path.join("css",y)
+        urlretrieve(x,outpath)
 
+def takeImage(url):
+    parsed = list(urlparse(url))
+    soup = takeHtmlContent(url)
+    for image in soup.find_all("img"):
+        # print("Image: %(src)s" % image)
+        filename = image["src"].split("/")[-1]
+        # print(image["src"])
+        parsed[2] = image["src"]
+        outpath = os.path.join("immagini", filename)
+        if image["src"].lower().startswith("http"):
+            urlretrieve(image["src"], outpath)
+        else:
+            urlretrieve(urlunparse(parsed), outpath)
 
 
